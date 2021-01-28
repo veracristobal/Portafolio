240fdde86b97a52aa855b9f0b309b2b19412082c
veracristobal <crizverar@gmail.com>
2021-01-28 13:23:46 -0400

    tercer commit

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,3 +1,27 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+
+  // Add smooth scrolling to all links
+  
+
+  
+
+  $('a').click(function(event){
+		if(this.hash !== ""){
+			event.preventDefault();
+
+		//GUARDAR ESE VALOR EN VARIIABLE GATO
+		var gato = this.hash;
+		//ANIMACION A TODO EL DOCUMENTO, 
+		$('html, body').animate({
+			scrollTop: $(gato).offset().top
+		}, 800, function(){
+			window.location.hash = gato;
+		});
+		
+		}
+	});
+
+ 	 $('[data-toggle="popover"]').popover();
+
+});
